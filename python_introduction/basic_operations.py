0a9279f3ecdef40926ca853311fa04bc57d2542c
tomibzan <thomflowen@gmail.com>
2025-05-22 21:56:14 +0300

    typo

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 total = number1 + number2
 difference = number1 - number2
 product = number1 * number2
-print("Addition of",number1, "and", number2, "is", sum)
+print("Addition of",number1, "and", number2, "is", total)
 print("Subtraction of", number1, "and", number2, "is", difference)
 print("Multiplication of", number1, "and", number2, "is", product)
