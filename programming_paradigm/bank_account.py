85caa6d6c3ed5edbd978e76baa4917ce4e20bded
tomibzan <thomflowen@gmail.com>
2025-06-10 20:47:53 +0300

    correction 1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,21 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
+    def __init__(self, account_balance=0.0):
         self.account_balance = account_balance
 
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-        
+        else:
+            print("Deposit amount must be positive.")
+
     def withdraw(self, amount):
-        if 0 < amount <= self.account_balance:
-            self.account_balance -= amount
-            return True
-        return False
-    
+        if amount <= 0:
+            print("Withdrawal amount must be positive.")
+            return False
+        if amount > self.account_balance:
+            return False
+        self.account_balance -= amount
+        return True
+
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
