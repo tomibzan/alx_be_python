7916514dcf7fc9a426f63825d5a6baa47f8396c4
tomibzan <thomflowen@gmail.com>
2025-06-21 09:45:27 +0300

    week7 polymorphism task

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses should implement this!")
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
